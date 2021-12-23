f11fe3168855edde22a06be4d8f1de90a547d7af
xd-101 <deltavictor28@gmail.com>
2021-12-23 01:46:51 +0000

    Add formatting and portal look

--- utils/util.py
+++ utils/util.py
@@ -1,7 +1,8 @@
-'''
+"""
 Precprocessing utility functions
 
-'''
+"""
+
 
 def select_columns(data_frame, column_names):
     new_frame = data_frame.loc[:, column_names]
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
-from dashboard import app
+from app import app
 
 server = app.server
 
