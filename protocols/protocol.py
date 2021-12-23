07259edaa1982accf509510c50722a6757fcebdd
Patrick D. Avery Jr <patrickdaj@gmail.com>
2021-12-23 11:20:22 -0800

    added protocol base class

---
+++ protocols/protocol.py
@@ -0,0 +1,86 @@
+import pandas as pd
+import plotly.express as px
+
+from dash import dcc, html
+
+
+class Protocol(object):
+    def __init__(self):
+        self.df = pd.read_csv(self.csv_url)
+        self.df = self.df.drop(self.df.columns[0], axis=1)[1:]
+        self.df["timestamp"] = pd.to_datetime(self.df["timestamp"], unit="s")
+
+    def preprocess(self):
+        self.supply_growth_df = self.select_columns(
+            self.df, ["timestamp", "supply"]
+        )
+        self.supply_growth_df["supply"] = self.supply_growth_df["supply"].apply(
+            lambda x: ((float(x) - 30000) / 30000) * 100
+        )
+        self.supply_growth_df.columns = ["timestamp", "growth-rate"]
+
+        self.dilution_df = self.select_columns(
+            self.supply_growth_df, ["timestamp", "growth-rate"]
+        )
+        self.dilution_df["growth-rate"] = (
+            self.dilution_df["growth-rate"] / self.df["index"]
+        )
+        self.dilution_df.columns = ["timestamp", "dilution"]
+
+    @staticmethod
+    def select_columns(data_frame, column_names):
+        new_frame = data_frame.loc[:, column_names]
+        return new_frame
+
+    def charts(self):
+        index_fig = px.line(self.df, x="timestamp", y="index", title="Index")
+        supply_growth_fig = px.line(
+            self.supply_growth_df,
+            x="timestamp",
+            y="growth-rate",
+            title="Supply Growth",
+        )
+        dilution_fig = px.line(
+            self.dilution_df, x="timestamp", y="dilution", title="% Dilution"
+        )
+        output = html.Div(
+            children=[
+                html.Div(
+                    [
+                        html.H2(
+                            children=[
+                                f"Wonderland {self.name} Dashboard",
+                            ],
+                            style={"text-align": "center"},
+                        ),
+                    ]
+                ),
+                html.Div(
+                    children=[
+                        html.Div(
+                            children=[
+                                html.Div(
+                                    children=[
+                                        dcc.Graph(
+                                            id="supply-growth",
+                                            figure=supply_growth_fig,
+                                        ),
+                                    ],
+                                ),
+                                html.Div(
+                                    children=[
+                                        dcc.Graph(id="dilution", figure=dilution_fig),
+                                    ],
+                                ),
+                                html.Div(
+                                    children=[
+                                        dcc.Graph(id="index", figure=index_fig),
+                                    ],
+                                ),
+                            ]
+                        ),
+                    ]
+                ),
+            ]
+        )
+        return output
